7c50e4d5c5530fe2916f3713501de2d386557063
uevitondev <uevitonsantos@outlook.com>
2023-03-09 16:28:17 -0300

    Database seeding

--- backend/src/main/java/com/uevitondev/catalog/services/CategoryService.java
+++ backend/src/main/java/com/uevitondev/catalog/services/CategoryService.java
@@ -4,6 +4,7 @@ import com.uevitondev.catalog.entities.Category;
 import com.uevitondev.catalog.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -13,6 +14,7 @@ public class CategoryService {
     @Autowired
     private CategoryRepository categoryRepository;
 
+    @Transactional(readOnly = true)
     public List<Category> findAllCategories() {
         return categoryRepository.findAll();
     }
