b5ed26602dc30c66fb6de07694f37d728db3a9dc
uevitondev <uevitonsantos@outlook.com>
2023-03-13 14:45:58 -0300

    Product insert, update, delete

--- backend/src/main/java/com/uevitondev/catalog/services/ProductService.java
+++ backend/src/main/java/com/uevitondev/catalog/services/ProductService.java
@@ -1,7 +1,10 @@
 package com.uevitondev.catalog.services;
 
+import com.uevitondev.catalog.dto.CategoryDTO;
 import com.uevitondev.catalog.dto.ProductDTO;
+import com.uevitondev.catalog.entities.Category;
 import com.uevitondev.catalog.entities.Product;
+import com.uevitondev.catalog.repository.CategoryRepository;
 import com.uevitondev.catalog.repository.ProductRepository;
 import com.uevitondev.catalog.services.exceptions.DatabaseException;
 import com.uevitondev.catalog.services.exceptions.ResourceNotFoundException;
@@ -20,6 +23,9 @@ public class ProductService {
     @Autowired
     private ProductRepository productRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     @Transactional(readOnly = true)
     public Page<ProductDTO> findAllProductsPaged(PageRequest pageRequest) {
         Page<Product> categoryPage = productRepository.findAll(pageRequest);
@@ -36,16 +42,17 @@ public class ProductService {
     @Transactional
     public ProductDTO insertProduct(ProductDTO productDTO) {
         Product product = new Product();
-        product.setName(productDTO.getName());
+        copyDtoToEntity(productDTO, product);
         product = productRepository.save(product);
-        return new ProductDTO(product);
+        return new ProductDTO(product, product.getCategories());
     }
+
 
     @Transactional
     public ProductDTO updateProduct(Long id, ProductDTO productDTO) {
         try {
             Product product = productRepository.getReferenceById(id);
-            product.setName(productDTO.getName());
+            copyDtoToEntity(productDTO, product);
             product = productRepository.save(product);
             return new ProductDTO(product);
         } catch (EntityNotFoundException e) {
@@ -60,6 +67,19 @@ public class ProductService {
             throw new ResourceNotFoundException("Id not found! id: " + id);
         } catch (DataIntegrityViolationException e) {
             throw new DatabaseException("Integrity violation!");
+        }
+    }
+
+    private void copyDtoToEntity(ProductDTO productDTO, Product product) {
+        product.setName(productDTO.getName());
+        product.setDescription(productDTO.getDescription());
+        product.setPrice(productDTO.getPrice());
+        product.setImgUrl(productDTO.getImgUrl());
+        product.setDate(productDTO.getDate());
+        product.getCategories().clear();
+        for (CategoryDTO categoryDTO : productDTO.getCategories()) {
+            Category category = categoryRepository.getReferenceById(categoryDTO.getId());
+            product.getCategories().add(category);
         }
     }
 
