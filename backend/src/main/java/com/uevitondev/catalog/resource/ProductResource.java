e46b8e2163c7c18a492e0a77ad5630caba4e205e
uevitondev <uevitonsantos@outlook.com>
2023-03-17 15:41:40 -0300

    Refactor pageable, new exceptio

--- backend/src/main/java/com/uevitondev/catalog/resource/ProductResource.java
+++ backend/src/main/java/com/uevitondev/catalog/resource/ProductResource.java
@@ -4,8 +4,7 @@ import com.uevitondev.catalog.dto.ProductDTO;
 import com.uevitondev.catalog.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -20,14 +19,8 @@ public class ProductResource {
     private ProductService productService;
 
     @GetMapping("/all")
-    public ResponseEntity<Page<ProductDTO>> findAllProductsPaged(
-            @RequestParam(value = "page", defaultValue = "0") Integer page,
-            @RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
-            @RequestParam(value = "direction", defaultValue = "ASC") String direction,
-            @RequestParam(value = "orderBy", defaultValue = "name") String orderBy
-    ) {
-        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
-        return ResponseEntity.ok().body(productService.findAllProductsPaged(pageRequest));
+    public ResponseEntity<Page<ProductDTO>> findAllProductsPaged(Pageable pageable) {
+        return ResponseEntity.ok().body(productService.findAllProductsPaged(pageable));
     }
 
     @GetMapping("/{id}")
