f3cc918f8970cb5263e2c6ca328a74a5db552b54
uevitondev <uevitonsantos@outlook.com>
2023-06-07 16:34:51 -0300

    custom validation response

--- backend/src/main/java/com/uevitondev/catalog/resources/exceptions/ControllerExceptionHandler.java
+++ backend/src/main/java/com/uevitondev/catalog/resources/exceptions/ControllerExceptionHandler.java
@@ -6,6 +6,8 @@ import com.uevitondev.catalog.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -52,4 +54,23 @@ public class ControllerExceptionHandler {
 
         return ResponseEntity.status(status).body(standardError);
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidationError> methodValidationException(MethodArgumentNotValidException e, HttpServletRequest servletRequest) {
+        HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+        ValidationError validationError = new ValidationError();
+        validationError.setTimestamp(Instant.now());
+        validationError.setStatus(status.value());
+        validationError.setError("validation exception!");
+        validationError.setMessage(e.getMessage());
+        validationError.setPath(servletRequest.getRequestURI());
+
+        for (FieldError error : e.getBindingResult().getFieldErrors()) {
+            validationError.addError(error.getField(), error.getDefaultMessage());
+        }
+
+        return ResponseEntity.status(status).body(validationError);
+    }
+
+
 }
