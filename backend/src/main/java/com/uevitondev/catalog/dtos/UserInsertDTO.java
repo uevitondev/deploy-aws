bb471c8c4620a8dfb4cf4739085a48269a4cec88
uevitondev <uevitonsantos@outlook.com>
2023-06-07 18:49:26 -0300

    Custom constraint validator

--- backend/src/main/java/com/uevitondev/catalog/dtos/UserInsertDTO.java
+++ backend/src/main/java/com/uevitondev/catalog/dtos/UserInsertDTO.java
@@ -1,5 +1,8 @@
 package com.uevitondev.catalog.dtos;
 
+import com.uevitondev.catalog.services.validation.UserInsertValid;
+
+@UserInsertValid
 public class UserInsertDTO extends UserDTO {
 
     private String password;
---
+++ backend/src/main/java/com/uevitondev/catalog/services/validation/UserInsertValidator.java
@@ -0,0 +1,42 @@
+package com.uevitondev.catalog.services.validation;
+
+import com.uevitondev.catalog.dtos.UserInsertDTO;
+import com.uevitondev.catalog.entities.User;
+import com.uevitondev.catalog.repositories.UserRepository;
+import com.uevitondev.catalog.resources.exceptions.FieldMessage;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserInsertValidator implements ConstraintValidator<UserInsertValid, UserInsertDTO> {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void initialize(UserInsertValid ann) {
+        // TODO
+    }
+
+    @Override
+    public boolean isValid(UserInsertDTO dto, ConstraintValidatorContext context) {
+
+        List<FieldMessage> list = new ArrayList<>();
+
+        User user = userRepository.findByEmail(dto.getEmail());
+
+        if (user != null) {
+            list.add(new FieldMessage("email", "Email já existe!"));
+
+        }
+
+        for (FieldMessage e : list) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName()).addConstraintViolation();
+        }
+        return list.isEmpty();
+    }
+}
